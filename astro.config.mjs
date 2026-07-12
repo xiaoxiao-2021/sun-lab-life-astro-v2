51a00659f72e388a6b50f49054c5006861e2aa1a
xiaoxiao-2021 <1346916132@qq.com>
2026-07-12 20:42:02 +0800

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://xiaoxiao-2021.github.io",
+  base: "/sun-lab-life-astro-v2",
+});
